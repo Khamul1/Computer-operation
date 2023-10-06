4abe2d77bd121421e48329bd873faf96f72fba6c
Khamul1 <rositlsav.kostencko@gmail.com>
2023-10-06 20:42:43 +0300

    Computer On,Off

---
+++ src/ru/javabegin/opp/computer/Main.java
@@ -0,0 +1,11 @@
+package ru.javabegin.opp.computer;
+
+public class Main {
+    public static void main(String[] arg){
+    Computer IBM = new Computer();
+    Computer.name = "IBM";
+    IBM.on();
+    IBM.off();
+
+    }
+        }
